2aeeece182b9522f81e7c30683e415ae2fc2424a
omair <omairdal@gmail.com>
2023-04-23 23:33:09 +0300

    fixed a silly mistake

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "components/layout/Navbar";
+import Navbar from "@/components/layout/Navbar/Navbar";
 import React from "react";
 import { observer } from "mobx-react-lite";
 import { Box } from "@mui/material";
