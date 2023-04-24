81c561d2df53226e6fa23d2259fbd5e4023c8c46
omair <omairdal@gmail.com>
2023-04-24 03:29:17 +0300

    meging

---
+++ pages/chores.tsx
@@ -0,0 +1,11 @@
+import { Stack, Typography } from "@mui/material";
+
+const Chores = () => {
+  return (
+    <Stack justifyContent="center" alignItems="center" sx={{ height: "calc(100% - 4rem)", width: "100%" }}>
+      <Typography>I&aspos;m a Chore</Typography>
+    </Stack>
+  );
+};
+
+export default Chores;
